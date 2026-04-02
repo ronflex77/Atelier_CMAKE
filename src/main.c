d105d6d38de1be35b2e79d4f7b1d7dcec43fd746
ronflex77 <116546506+ronflex77@users.noreply.github.com>
2026-04-02 12:12:03 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -3,33 +3,52 @@
 #include <string.h>
 #include "./../include/calculator.h"
 
-int main(int argc, char *argv[]){
-	if (argc == 4)
-	{
-	char* op = argv[1];
-	char* a = argv[2];
-	char* b = argv[3];
-	double r = 0;
-	if (strcmp(op, "add") == 0) {
-		r = _add(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else if (strcmp(op, "sub") == 0) {
-		r = _sub(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else if (strcmp(op, "mul") == 0) {
-		r = _mul(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else if (strcmp(op, "div") == 0) {
-		r = _div(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else {printf("Erreur de parametres");}
-	}
-	else {printf("Erreur de parametres");}
-	return 0;
-}
+int main(int argc, char *argv[]) {
+    char* op;
+    double r = 0;
 
-		
+    if (argc < 3) {
+        printf("Erreur de parametres");
+        return 0;
+    }
+
+    op = argv[1];
+
+    if (strcmp(op, "car") == 0) {
+        if (argc != 3) {
+            printf("Erreur de parametres");
+            return 0;
+        }
+
+        r = _car(atof(argv[2]));
+        printf("%lf", r);
+    }
+    else {
+        if (argc != 4) {
+            printf("Erreur de parametres");
+            return 0;
+        }
+
+        if (strcmp(op, "add") == 0) {
+            r = _add(atof(argv[2]), atof(argv[3]));
+            printf("%lf", r);
+        }
+        else if (strcmp(op, "sub") == 0) {
+            r = _sub(atof(argv[2]), atof(argv[3]));
+            printf("%lf", r);
+        }
+        else if (strcmp(op, "mul") == 0) {
+            r = _mul(atof(argv[2]), atof(argv[3]));
+            printf("%lf", r);
+        }
+        else if (strcmp(op, "div") == 0) {
+            r = _div(atof(argv[2]), atof(argv[3]));
+            printf("%lf", r);
+        }
+        else {
+            printf("Erreur de parametres");
+        }
+    }
+
+    return 0;
+}
